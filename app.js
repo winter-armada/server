94bf70bdd82b2b0c6adfd975f2b1790cf934e88c
roberthumphrey <rhumphrey163@gmail.com>
2019-02-15 22:50:57 -0600

    shitty shit shit shit

---
+++ app.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const mongoose = require('mongoose');
+const client = new Discord.Client();
+require('dotenv');
+
+// initialize database shit here
+
+// do ur shitty bot shit here
+
+// login ur shitty bot here
